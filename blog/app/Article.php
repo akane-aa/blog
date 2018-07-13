2292a0631b5913a5250d230a14f1c22a12eb53b9
阿部茜 <abeakane@ibm-2dd9a2792d6.people.local>
2018-07-13 09:40:29 +0900

    add_laravel_20

--- blog/app/Article.php
+++ blog/app/Article.php
@@ -2,10 +2,14 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
     protected $fillable = ['title', 'body', 'published_at'];
-    // protected $fillable = [$inputs];
+
+    public function scopePublished($query){
+        $query->where('published_at','<=',Carbon::now());
+    }
 }
