b8401c8164e3a685f3ee6823505f62dabcbba91c
阿部茜 <abeakane@ibm-2dd9a2792d6.people.local>
2018-07-20 09:53:26 +0900

    add_laravel_25

--- blog/resources/views/articles/create.blade.php
+++ blog/resources/views/articles/create.blade.php
@@ -7,23 +7,9 @@
     <hr/>
 
     @include('errors.form_errors')
-    <!-- { Form::open(['url' => 'articles']) } -->
+
     {!! Form::open(['route' => 'articles.store']) !!}
          @include('articles.form', ['published_at' => date('Y-m-d'), 'submitButton' => 'Add Article'])
-        <!-- <div class="form-group">
-            {!! Form::label('title', 'Title:') !!}
-            {!! Form::text('title', null, ['class' => 'form-control']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('body', 'Body:') !!}
-            {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::label('published_at', 'Publish On:') !!}
-            {!! Form::input('date', 'published_at', date('Y-m-d'), ['class' => 'form-control']) !!}
-        </div>
-        <div class="form-group">
-            {!! Form::submit('Add Article', ['class' => 'btn btn-primary form-control']) !!}
-        </div> -->
+        
     {!! Form::close() !!}
 @endsection
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,24 +11,4 @@
 |
 */
 
-//Route::get('/', function () {
-    //return view('welcome');
-//});
-// Route::get('/', 'WelcomeController@index');
-// Route::get('contact', 'WelcomeController@contact');
-// Route::get('about', 'PagesController@about');
-// Route::get('articles', 'ArticlesController@index');
-// Route::get('articles/create', 'ArticlesController@create');
-// Route::get('articles/{id}', 'ArticlesController@show');
-// Route::post('articles', 'ArticlesController@store');
-// Route::get('articles/{id}/edit','ArticlesController@edit');
-// Route::patch('articles/{id}','ArticlesController@update');
-// Route::delete('articles/{id}', 'ArticlesController@destroy');
-
-Route::get('articles', ['as' => 'articles.index', 'uses' => 'ArticlesController@index']);
-Route::get('articles/create', ['as' => 'articles.create', 'uses' => 'ArticlesController@create']);
-Route::get('articles/{id}', ['as' => 'articles.show', 'uses' => 'ArticlesController@show']);
-Route::post('articles', ['as' => 'articles.store', 'uses' => 'ArticlesController@store']);
-Route::get('articles/{id}/edit', ['as' => 'articles.edit', 'uses' => 'ArticlesController@edit']);
-Route::patch('articles/{id}', ['as' => 'articles.update', 'uses' => 'ArticlesController@update']);
-Route::delete('articles/{id}', ['as' => 'articles.destroy', 'uses' => 'ArticlesController@destroy']);
+Route::resource('articles','ArticlesController');
