b61048d83704bf8b54e740e5d6b2712180b45fa9
阿部茜 <abeakane@ibm-2dd9a2792d6.people.local>
2018-07-23 13:49:32 +0900

    add_larave_28

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # blog
+
+laravel_28_参照
+Authcontrollerはlaravel5.3から以下の４つのコントローラーに分けられる。
+
+LoginController.php
+RegisterController.php
+ForgotPasswordController.php
+ResetPasswordController.php
+
+Laravel 5.5 の認証機能とそのカスタマイズ
+https://qiita.com/naga3/items/4f3defde59b31a1a797e
+このqiitaの「追加されたルート」に記載されている各コントローラーのメソッドが
+ララ帳には記載されていないので覚えておくべき
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,17 +11,27 @@
 |
 */
 Route::get('about','PagesController@about');
-Route::get('contact','PagesController@contact');
+Route::get('contact','WelcomeController@contact');
 
 Route::get('/','ArticlesController@index');
 
 Route::resource('articles','ArticlesController');
 
 //Authentication routes
-Route::get('auth/login','Auth\AuthController@getLogin');
-Route::post('auth/login','Auth\AuthController@postLogin');
-Route::get('auth/logout','Auth\AuthController@getLogout');
+Route::get('auth/login','Auth\LoginController@showLoginForm');
+Route::post('auth/login','Auth\LoginController@login');
+Route::get('auth/logout','Auth\LoginController@logout');
 
 //Registration routes
-Route::get('auth/register','Auth\AuthController@getRegister');
-Route::post('auth/register','Auth\AuthController@postRegister');
+Route::get('auth/register','Auth\RegisterController@showRegistrationForm');
+Route::post('auth/register','Auth\RegisterController@register');
+
+// Password reset link request routes...
+
+//laravel_29
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+
+// Password reset routes...
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
